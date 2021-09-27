73c819c7c221423bc6d4075245f68cf8ee5fa2d8
Thalles Rosa <labufes@gmail.com>
2021-09-27 09:39:48 -0300

    Correção do caminho do README

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ TEST_REQUIREMENTS = (
 
 setup(
     name='rest_framework_admin_api',
-    version='0.0.4',
+    version='0.0.6',
     packages=['rest_framework_admin_api'],
     long_description=open('./README.md', 'rt').read(),
     long_description_content_type="text/markdown",
