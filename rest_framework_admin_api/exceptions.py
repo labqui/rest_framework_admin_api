d2d7bd9e0505fddca24d35b250ad7898729e31c7
Thalles Rosa <labufes@gmail.com>
2021-10-02 20:38:11 -0300

    Correção do caminho do README

--- rest_framework_admin_api/exceptions.py
+++ rest_framework_admin_api/exceptions.py
@@ -1,8 +1,34 @@
+import re
+
 from rest_framework.views import exception_handler
+from django.http import Http404
+from rest_framework import exceptions
+from rest_framework.exceptions import PermissionDenied, ValidationError
+from rest_framework.response import Response
+from rest_framework.views import set_rollback
 
 
 def admin_api_exception_handler(exc, context):
+    if isinstance(exc, Http404):
+        exc = exceptions.NotFound()
+    elif isinstance(exc, PermissionDenied):
+        exc = exceptions.PermissionDenied()
+
+    if isinstance(exc, exceptions.APIException):
+        headers = {}
+        if isinstance(exc, ValidationError) and exc.status_code == 400:
+            data = {
+                'type': re.sub(r'(?<!^)(?=[A-Z])', '_', str(ValidationError.__name__)).lower(),
+                'errors': exc.detail
+            }
+        elif isinstance(exc.detail, (list, dict)):
+            data = exc.detail
+        else:
+            data = {
+                'detail': exc.detail
+            }
+
+        set_rollback()
+        return Response(data, status=exc.status_code, headers=headers)
     response = exception_handler(exc, context)
-    # if response is not None:
-    #     response.data['status_code'] = response.status_code
     return response
